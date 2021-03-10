805417c6364372fa3ba7dd6f47d941e5ef8c9c5f
AnnaKoliada <anacirec@gmail.com>
2021-03-10 14:39:48 +0300

    feat: Add decision of task and add tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    num = String(parseInt(n));
+    num = num.split("").reverse().join("");
+    number = String(parseInt(num));
+    return number;
+};
